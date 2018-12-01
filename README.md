923ff812ad656a58054c4eb2870746a4573c845f
Joachim Dalen <me@d3v.no>
2018-12-01 22:20:33 +0100

    Add source code

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # notes-combiner
+
+All my notes are written in Markdown using [Boostnote](https://boostnote.io/).
+This script is used to take all markdown files in a directory, and combine them into a single PDF file.
+
+### Usage
+All you need to do is change two variables in `index.js`
+
+- `outputPath` : Output path where PDF should be saved to. E.g. `./notes.pdf`
+- `markdownFilesDir` : Folder containing markdown files to combine. E.g. `./notes`
+
+Then run `node index.js`
+
+
+*This was just made to be quick way to combine the files, hence the manual source code changes needed.*
---
+++ index.js
@@ -0,0 +1,39 @@
+const markdownpdf = require("markdown-pdf");
+const through = require("through");
+const fs = require("fs");
+const path = require("path");
+
+const outputPath = "";
+const markdownFilesDir = "";
+function extension(element) {
+  var extName = path.extname(element);
+  return extName === ".md";
+}
+
+function preProcessMd() {
+  return through(function(data) {
+    pageBreak = '\n\n <div style="page-break-after:always"></div> \n\n';
+    this.queue(data + pageBreak);
+  });
+}
+var mdDocs = fs
+  .readdirSync(markdownFilesDir)
+  .filter(extension)
+  .map(f => {
+    return path.join(markdownFilesDir, f);
+  });
+
+const options = {
+  paperFormat: "A4",
+  preProcessMd: preProcessMd,
+  cssPath: "./styles.css",
+  remarkable: {
+    html: true
+  }
+};
+
+markdownpdf(options)
+  .concat.from(mdDocs)
+  .to(outputPath, function() {
+    console.log("Created output file", outputPath);
+  });
